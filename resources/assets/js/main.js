ae7ae6a5f2d5bf7b0c403934884d4d8ad763b90a
fazemii01 <bestvatp@gmail.com>
2026-06-29 19:41:26 +0700

    gtw

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -612,5 +612,20 @@ if (typeof $ !== 'undefined') {
                 psSearch.update();
             });
         }
+
+        // Fix Z-Index and Stacking for Dropdowns in Tables / Animated Lists
+        $(document).on('show.bs.dropdown', '.dropdown', function () {
+            var item = $(this).closest('tr, .animate__animated');
+            if (item.length) {
+                item.css({ 'z-index': 1050, 'position': 'relative' });
+            }
+            $(this).closest('.card, .table-responsive').css('overflow', 'visible');
+        });
+        $(document).on('hide.bs.dropdown', '.dropdown', function () {
+            var item = $(this).closest('tr, .animate__animated');
+            if (item.length) {
+                item.css({ 'z-index': '', 'position': '' });
+            }
+        });
     });
 }
